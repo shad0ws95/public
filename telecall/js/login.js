9b9754ede9bf0413d8901da75f26ed6da0a7a57e
Felipe Guedes <87718560+shad0ws95@users.noreply.github.com>
2023-12-06 13:43:57 -0300

    Update login.js

--- telecall/js/login.js
+++ telecall/js/login.js
@@ -1,1 +1,32 @@
+function entrar(){
+    let login = document.querySelector("#login");
+    let senha = document.querySelector("#senha");
+  
+    let listaUsuario = [];
+  
+    let validaUsuario = {
+    login: '',
+    senha: ''
+    }
+    listaUsuario = JSON.parse(localStorage.getItem('listaUsuario'));
 
+    listaUsuario.forEach((item) => {
+        if(login.value == item.login && senha.value == item.senha){
+            validaUsuario = {
+                login: item.login,
+                senha: item.senha
+            }
+        }
+            
+        })
+        if(login.value == validaUsuario.login && senha.value == validaUsuario.senha){
+            let mathRandom = Math.random().toString(16).substring(2)
+            let token = mathRandom + mathRandom
+            localStorage.setItem('token', token)
+            localStorage.setItem('userLogado', JSON.stringify(validaUsuario))
+            alert("Seja bem vindo!");
+            window.location.href = 'logado.html'
+        } else(
+            alert("Usuário ou senha incorretos")
+        )
+}
